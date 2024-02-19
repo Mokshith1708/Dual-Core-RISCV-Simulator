35d2e3518e21052c2e2e31837bb6a52c3053d7c0
Mokshith1708 <146767844+Mokshith1708@users.noreply.github.com>
2024-02-19 21:28:10 +0530

    changes

--- simulator/simulator.hpp
+++ simulator/simulator.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstdint>
 #include <cstdarg>
+#include <bits/stdc++.h>
 namespace RISCV
 {
     enum reg
@@ -65,122 +66,7 @@ namespace RISCV
 
     };
 }
-std::string InstToString(RISCV::Inst inst)
-{
-    switch (inst)
-    {
-    case RISCV::jal:
-        return "jal";
-    case RISCV::jalr:
-        return "jalr";
-    case RISCV::beq:
-        return "beq";
-    case RISCV::bne:
-        return "bne";
-    case RISCV::blt:
-        return "blt";
-    case RISCV::bge:
-        return "bge";
-    case RISCV::bltu:
-        return "bltu";
-    case RISCV::begu:
-        return "begu";
-    case RISCV::lb:
-        return "lb";
-    case RISCV::lw:
-        return "lw";
-    case RISCV::sb:
-        return "sb";
-    case RISCV::sw:
-        return "sw";
-    case RISCV::addi:
-        return "addi";
-    case RISCV::add:
-        return "add";
-    case RISCV::sub:
-        return "sub";
-    case RISCV::ecall:
-        return "ecall";
-    case RISCV::lbu:
-        return "lbu";
-    case RISCV::lwu:
-        return "lwu";
-    default:
-        return "Unknown";
-    }
-}
-std::string regToString(RISCV::reg r)
-{
-    switch (r)
-    {
-    case RISCV::x0:
-        return "x0";
-    case RISCV::x1:
-        return "x1";
-    case RISCV::x2:
-        return "x2";
-    case RISCV::x3:
-        return "x3";
-    case RISCV::x4:
-        return "x4";
-    case RISCV::x5:
-        return "x5";
-    case RISCV::x6:
-        return "x6";
-    case RISCV::x7:
-        return "x7";
-    case RISCV::x8:
-        return "x8";
-    case RISCV::x9:
-        return "x9";
-    case RISCV::x10:
-        return "x10";
-    case RISCV::x11:
-        return "x11";
-    case RISCV::x12:
-        return "x12";
-    case RISCV::x13:
-        return "x13";
-    case RISCV::x14:
-        return "x14";
-    case RISCV::x15:
-        return "x15";
-    case RISCV::x16:
-        return "x16";
-    case RISCV::x17:
-        return "x17";
-    case RISCV::x18:
-        return "x18";
-    case RISCV::x19:
-        return "x19";
-    case RISCV::x20:
-        return "x20";
-    case RISCV::x21:
-        return "x21";
-    case RISCV::x22:
-        return "x22";
-    case RISCV::x23:
-        return "x23";
-    case RISCV::x24:
-        return "x24";
-    case RISCV::x25:
-        return "x25";
-    case RISCV::x26:
-        return "x26";
-    case RISCV::x27:
-        return "x27";
-    case RISCV::x28:
-        return "x28";
-    case RISCV::x29:
-        return "x29";
-    case RISCV::x30:
-        return "x30";
-    case RISCV::x31:
-        return "x31";
-    default:
-        return "Unknown";
-    }
-}
+
 bool isBranch(int i)
 {
     if (i == 3 || i == 4 || i == 5 || i == 6 ||
