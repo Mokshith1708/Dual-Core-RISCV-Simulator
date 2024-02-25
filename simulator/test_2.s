9d1e05f70d7113758afe793b0ead5ac657901e1d
Mokshith1708 <146767844+Mokshith1708@users.noreply.github.com>
2024-02-25 09:55:33 +0530

    made some changes

--- simulator/test_2.s
+++ simulator/test_2.s
@@ -11,12 +11,12 @@ addi x7 x7 -1
 loop2:
 addi x1 x1 1
 lw x4 0(x5)
-lw x9 1(x5)
+lw x9 4(x5)
 blt x4 x9 noswap 
-sw x4 1(x5)
+sw x4 4(x5)
 sw x9 0(x5)
 noswap:
-addi x5 x5 1
+addi x5 x5 4
 bne x1 x8 loop2
 add x1 x0 x0
 add x5 x6 x0
