b4bd3c37ea5bd4b06142dccaead4f78ea94ba8f4
Mokshith1708 <146767844+Mokshith1708@users.noreply.github.com>
2024-03-03 22:13:44 +0530

    Implemented pipelining by adding all the five stages seperately. We achieved it using Backward execution.

--- Pipelined/cores.cpp
+++ Pipelined/cores.cpp
@@ -3,131 +3,413 @@
 #include "memory.hpp"
 #include "registers.hpp"
 // Constructor for ALU class
+ALU::ALU()
+    : fetch(2, std::vector<int>()),
+      decode(2, std::vector<int>()),
+      execute(2, std::vector<int>()),
+      mem(2, std::vector<int>()),
+      write(2, std::vector<int>())
+{
+}
 ALU::ALU(std::pair<int, int> &p1, std::pair<int, int> &p2, int no_inst_1, int no_inst_2, memory &m, registers &r1, registers &r2, int core1, int core2)
 {
     pc1 = p1.second;
     pc2 = p2.second;
 
     int maxim = std::max(no_inst_1, no_inst_2);
+    std::vector<int>v,k;
     while (pc1 < maxim && pc2 < maxim)
     {
         // std::cout << no_inst << std::endl;
         if (pc1 < no_inst_1)
         {
-            //executeInstruction(m.read_instruction(pc1, core1), m, r1, core1, pc1);
-            writeBack(m,core1, pc1);
+            // executeInstruction(m.read_instruction(pc1, core1), m, r1, core1, pc1);
+            writeBack(k,m,core1, pc1,r1);
+            memoryAccess(k,m,core1,pc1);
+            k=instructionExecute(v,m,r1,core1,pc1);
+            v=instructionDecode(m,core1,r1,pc1);
+            instructionFetch(m,core1,pc1);
+            clockCycles1++;
+            std::cout<<clockCycles1<<std::endl;
         }
         if (pc2 < no_inst_2)
         {
-            executeInstruction(m.read_instruction(pc2, core2), m, r2, core2, pc2);
+            writeBack(k,m,core2, pc2,r2);
+            memoryAccess(k,m,core2,pc2);
+            k=instructionExecute(v,m,r2,core2,pc2);
+            v=instructionDecode(m,core2,r2,pc2);
+            instructionFetch(m,core2,pc2);
+            clockCycles2++;
         } //  pc++;
     }
 }
 
-void ALU::instructionFetch(memory &m, int core, int &pc)
-{    
-    fetch[core-1].push_back(m.read_instruction(pc, core));
+void ALU::instructionFetch(memory &m, int core, int& pc)
+{   
+    // fetch[core - 1].push_back(m.read_instruction(pc, core));
+    // pc++;
+    std::vector<int> instruction = m.read_instruction(pc, core);
+    fetch[core - 1].insert(fetch[core - 1].end(), instruction.begin(), instruction.end());
     pc++;
 }
 
-void ALU::instructionDecode(memory &m, int core)
-{
-    if (fetch[core-1].empty())
-    {
-        return;
-    }
-    for (const auto &temp : fetch[core - 1])
+std::vector<int> ALU::instructionDecode(memory &m, int core,registers& r,int& pc)
+{   std::vector<int>v;
+    if (decode[core - 1].empty())
+    { for (auto &temp : fetch[core - 1])
     {
         decode[core - 1].push_back(temp);
     }
     fetch[core - 1].clear();
-}
-void ALU::instructionExecute(memory &m, registers &r, int core, int &pc)
-{
-    if (execute[core-1].empty())
-    {
-        return;
+        return v;
     }
-    if (execute[core-1][1] == 0 && execute[core-1][2] == 0 && execute[3] == 0)
-    {
-        return;
-    }
-    if (execute[0] == -1 || execute[0] == -2)
-    {
-        return;
-    }
-    // Convert the execute opcode to enum type
-    RISCV::Inst opcode = static_cast<RISCV::Inst>(execute[core-1][0]);
+    
+
+   // std::vector<int>v;
+    RISCV::Inst opcode = static_cast<RISCV::Inst>(decode[core - 1][0]);
     switch (opcode)
     {
     case RISCV::add:
     {
-        int rd = execute[core-1][1];
-        int rs1 = execute[core-1][2];
-        int rs2 = execute[core-1][3];
+        int rd = decode[core - 1][1];
+        int rs1 = decode[core - 1][2];
+        int rs2 = decode[core - 1][3];
         int ans1 = r.read(rs1);
         int ans2 = r.read(rs2);
-        r.write(rd, ans1 + ans2);
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(ans1);
+        v.push_back(ans2);
+       // r.write(rd, ans1 + ans2);
         break;
     }
     case RISCV::sub:
     {
-        int rd = execute[1];
-        int rs1 = execute[2];
-        int rs2 = execute[3];
-        r.write(rd, r.read(rs1) - r.read(rs2));
+        int rd = decode[core - 1][1];
+        int rs1 = decode[core - 1][2];
+        int rs2 = decode[core - 1][3];
+       // r.write(rd, r.read(rs1) - r.read(rs2));
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(r.read(rs2));
         break;
     }
     case RISCV::mul:
     {
-        int rd = execute[1];
-        int rs1 = execute[2];
-        int rs2 = execute[3];
-        r.write(rd, r.read(rs1) * r.read(rs2));
+        int rd = decode[core - 1][1];
+        int rs1 = decode[core - 1][2];
+        int rs2 = decode[core - 1][3];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(r.read(rs2));
+       // r.write(rd, r.read(rs1) * r.read(rs2));
         break;
     }
     case RISCV::addi:
     {
-        int rd = execute[1];
-        int rs1 = execute[2];
-        int temp2 = execute[3];
+        int rd = decode[core - 1][1];
+        int rs1 = decode[core - 1][2];
+        int temp2 = decode[core - 1][3];
         // std::cout<<rd<<" "<<r.read(rs1)<<" "<<temp2<<std::endl;
-        r.write(rd, r.read(rs1) + temp2);
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(temp2);
+      //  r.write(rd, r.read(rs1) + temp2);
         break;
     }
     case RISCV::muli:
     {
-        int rd = execute[1];
-        int rs1 = execute[2];
-        int temp2 = execute[3];
+        int rd = decode[core - 1][1];
+        int rs1 = decode[core - 1][2];
+        int temp2 = decode[core - 1][3];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(temp2);
         // std::cout<<rd<<" "<<r.read(rs1)<<" "<<temp2<<std::endl;
-        r.write(rd, r.read(rs1) * temp2);
+       // r.write(rd, r.read(rs1) * temp2);
         break;
     }
     case RISCV::jal:
     {
-        int rd = execute[1];
-        int rs1 = execute[2];
-        r.write(rd, pc + 1);
-        pc = rs1;
+        int rd = decode[core - 1][1];
+        int rs1 = decode[core - 1][2];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(rs1);
+       // v.push_back();
+       // r.write(rd, pc + 1);
+       // pc = rs1;
         break;
     }
     case RISCV::jalr:
     {
-        int rd = execute[1];
-        int rs1 = r.read(execute[2]);
-        int offset = execute[3];
-        r.write(rd, pc + 1);
+        int rd = decode[core - 1][1];
+        int rs1 = r.read(decode[core - 1][2]);
+        int offset = decode[core - 1][3];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(rs1);
+        v.push_back(offset);
+      //  r.write(rd, pc + 1);
         // std::cout << rs1 << std::endl;
-        pc = rs1 + offset - 1;
+      //  pc = rs1 + offset - 1;
         // std::cout << pc << std::endl;
         break;
     }
     case RISCV::beq:
     {
-        int rs1 = execute[1];
-        int rs2 = execute[2];
-        int rd = execute[3];
+        int rs1 = decode[core - 1][1];
+        int rs2 = decode[core - 1][2];
+        int rd = decode[core - 1][3];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(r.read(rs2));
+        break;
+        // if (r.read(rs1) == r.read(rs2))
+        // {
+        //     pc = rd;
+        //     break;
+        // }
+        // else
+        // {
+        //     break;
+        // }
+    }
+    case RISCV::bne:
+    {
+        int rs1 = decode[core - 1][1];
+        int rs2 = decode[core - 1][2];
+        int rd = decode[core - 1][3];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(r.read(rs2));
+        break;
+        // if (r.read(rs1) != r.read(rs2))
+        // {
+        //     pc = rd;
+        //     break;
+        // }
+        // else
+        // {
+        //     break;
+        // }
+    }
+    case RISCV::blt:
+    {
+        int rs1 = decode[core - 1][1];
+        int rs2 = decode[core - 1][2];
+        int rd = decode[core - 1][3];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(r.read(rs2));
+        // if (r.read(rs1) < r.read(rs2))
+        // {
+        //     pc = rd;
+        //     break;
+        // }
+        // else
+        // {
+        //     break;
+        // }
+        break;
+    }
+    case RISCV::bge:
+    {
+        int rs1 = decode[core - 1][1];
+        int rs2 = decode[core - 1][2];
+        int rd = decode[core - 1][3];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(r.read(rs2));
+        // if (r.read(rs1) >= r.read(rs2))
+        // {
+        //     pc = rd;
+        //     break;
+        // }
+        // else
+        // {
+        //     break;
+        // }
+        break;
+    }
+    case RISCV::j:
+    {
+        int rd = decode[core - 1][1];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        // std::cout<<rd<<std::endl;
+       // pc = rd;
+        break;
+    }
+    case RISCV::lw:
+    {
+        int rd = decode[core - 1][1];
+        int rs1 = r.read(decode[core - 1][3]);
+        int offset = decode[core - 1][2];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(rs1);
+        v.push_back(offset);
+       // r.write(rd, m.read_memory((rs1 + offset) / 4, core));
+        break;
+    }
+    case RISCV::sw:
+    {
+        int rs1 = decode[core - 1][1];
+        int rd = r.read(decode[core - 1][3]);
+        int offset = decode[core - 1][2];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(r.read(rs1));
+        v.push_back(offset);
+        //m.write_memory((rd + offset) / 4, r.read(rs1), core);
+        break;
+    }
+    case RISCV::la:
+    {
+        int rd = decode[core - 1][1];
+        std::vector<int> v11 = m.read_instruction(decode[core - 1][2], core);
+        int rs1 = v11[1];
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(rs1);
+        //r.write(rd, rs1);
+        // std::cout << rs1 << std::endl;
+        break;
+    }
+    case RISCV::li:
+    {
+        int rd = decode[core - 1][1];
+        int rs1 = decode[core - 1][2];
+        // std::cout << rs1 << " " << rd << std::endl;
+        v.push_back(decode[core - 1][0]);
+        v.push_back(rd);
+        v.push_back(rs1);
+        r.write(rd, rs1);
+        break;
+    }
+    case RISCV::ecall:
+    {
+        int n = r.read(17);
+        int k = r.read(10);
+        v.push_back(decode[core - 1][0]);
+        v.push_back(n);
+        v.push_back(k);
+        // if (n == 1)
+        // {
+        //     std::cout << k;
+        // }
+        // if (n == 4)
+        // {
+        //     std::string st_temp = m.read_str(k, core);
+        //     std::cout << st_temp.substr(1, st_temp.size() - 2);
+        // }
+        break;
+    }
+    default:
+        break;
+    }
+    for (auto &temp : fetch[core - 1])
+    {
+        decode[core - 1].push_back(temp);
+    }
+    fetch[core - 1].clear();
+     return v;
+}
+std::vector<int> ALU::instructionExecute(std::vector<int>v,memory &m, registers &r, int core, int &pc)
+{
+
+    std::vector<int>k;
+    if (execute[core - 1].empty())
+    {
+    execute[core - 1] = decode[core - 1];
+    decode[core - 1].clear();
+    return k;
+    }
+    if (execute[core - 1][1] == 0 && execute[core - 1][2] == 0 && execute[core-1][3] == 0)
+    {
+        return k;
+    }
+    if (execute[core - 1][0] == -1 || execute[core - 1][0] == -2)
+    {
+        return k;
+    }
+    
+    // Convert the execute opcode to enum type
+    RISCV::Inst opcode = static_cast<RISCV::Inst>(v[0]);
+    switch (opcode)
+    {
+    case RISCV::add:
+    {
+        int ans = v[2]+v[3];
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(ans);
+        //r.write(rd, ans1 + ans2);
+        break;
+    }
+    case RISCV::sub:
+    {
+        int ans = v[2]-v[3];
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(ans);
+        break;
+    }
+    case RISCV::mul:
+    {
+       int ans = v[2]*v[3];
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(ans);
+        break;
+    }
+    case RISCV::addi:
+    {
+        int ans = v[2]+v[3];
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(ans);
+        break;
+    }
+    case RISCV::muli:
+    {
+        int ans = v[2]*v[3];
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(ans);
+        break;
+    }
+    case RISCV::jal:
+    { 
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(pc+1);
+       // r.write(rd, pc + 1);
+        pc = v[2];
+        break;
+    }
+    case RISCV::jalr:
+    {
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(pc+1);
+        pc = v[2] + v[3] - 1;
+        break;
+    }
+    case RISCV::beq:
+    {
+        int rs1 = v[2];
+        int rs2 = v[3];
+        int rd = v[1];
         if (r.read(rs1) == r.read(rs2))
         {
             pc = rd;
@@ -140,9 +422,9 @@ void ALU::instructionExecute(memory &m, registers &r, int core, int &pc)
     }
     case RISCV::bne:
     {
-        int rs1 = execute[1];
-        int rs2 = execute[2];
-        int rd = execute[3];
+        int rs1 = v[2];
+        int rs2 = v[3];
+        int rd = v[1];
         if (r.read(rs1) != r.read(rs2))
         {
             pc = rd;
@@ -155,9 +437,9 @@ void ALU::instructionExecute(memory &m, registers &r, int core, int &pc)
     }
     case RISCV::blt:
     {
-        int rs1 = execute[1];
-        int rs2 = execute[2];
-        int rd = execute[3];
+        int rs1 = v[2];
+        int rs2 = v[3];
+        int rd = v[1];
         if (r.read(rs1) < r.read(rs2))
         {
             pc = rd;
@@ -170,9 +452,9 @@ void ALU::instructionExecute(memory &m, registers &r, int core, int &pc)
     }
     case RISCV::bge:
     {
-        int rs1 = execute[1];
-        int rs2 = execute[2];
-        int rd = execute[3];
+        int rs1 = v[2];
+        int rs2 = v[3];
+        int rd = v[1];
         if (r.read(rs1) >= r.read(rs2))
         {
             pc = rd;
@@ -185,48 +467,50 @@ void ALU::instructionExecute(memory &m, registers &r, int core, int &pc)
     }
     case RISCV::j:
     {
-        int rd = execute[1];
+        int rd = v[1];
         // std::cout<<rd<<std::endl;
         pc = rd;
         break;
     }
     case RISCV::lw:
     {
-        int rd = execute[1];
-        int rs1 = r.read(execute[3]);
-        int offset = execute[2];
-        r.write(rd, m.read_memory((rs1 + offset) / 4, core));
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(m.read_memory((v[2] + v[3])/4,core));
+       // r.write(rd, m.read_memory((rs1 + offset) / 4, core));
         break;
     }
     case RISCV::sw:
     {
-        int rs1 = execute[1];
-        int rd = r.read(execute[3]);
-        int offset = execute[2];
-        m.write_memory((rd + offset) / 4, r.read(rs1), core);
+        k.push_back(1);
+        k.push_back(v[1]+v[3]/4);
+        k.push_back(v[2]);
         break;
     }
     case RISCV::la:
     {
-        int rd = execute[1];
-        std::vector<int> v = m.read_instruction(execute[2], core);
-        int rs1 = v[1];
-        r.write(rd, rs1);
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(v[2]);
+        // r.write(rd, rs1);
         // std::cout << rs1 << std::endl;
         break;
     }
     case RISCV::li:
     {
-        int rd = execute[1];
-        int rs1 = execute[2];
+        // int rd = execute[core - 1][1];
+        // int rs1 = execute[core - 1][2];
+        k.push_back(0);
+        k.push_back(v[1]);
+        k.push_back(v[2]);
         // std::cout << rs1 << " " << rd << std::endl;
-        r.write(rd, rs1);
+       // r.write(rd, rs1);
         break;
     }
     case RISCV::ecall:
     {
-        int n = r.read(17);
-        int k = r.read(10);
+        int n = v[1];
+        int k = v[2];
         if (n == 1)
         {
             std::cout << k;
@@ -241,26 +525,40 @@ void ALU::instructionExecute(memory &m, registers &r, int core, int &pc)
     default:
         break;
     }
-    execute = decode;
-    decode.clear();
+    execute[core - 1] = decode[core - 1];
+    decode[core - 1].clear();
+    return k;
 }
 
-void ALU::memoryAccess(memory& m, int core, int& pc)
+void ALU::memoryAccess(std::vector<int>k, memory &m, int core, int &pc)
 {
-    if (mem.empty())
-    {
+    if (mem[core - 1].empty())
+    {   
+        mem[core - 1] = execute[core - 1];
+        execute[core - 1].clear();
         return;
     }
-    mem=execute;
-    execute.clear();
+    if(k[0]==1)
+    {
+      m.write_memory(k[1],k[2],core);
+    }
+
+    mem[core - 1] = execute[core - 1];
+    execute[core - 1].clear();
 }
 
-void ALU::writeBack(memory& m, int core, int& pc)
+void ALU::writeBack(std::vector<int>k,memory &m, int core, int &pc, registers &r)
 {
-    if(write.empty())
+    if (write[core - 1].empty())
     {
-        return;
+       write[core - 1] = mem[core - 1];
+       mem[core - 1].clear();
+       return;
     }
-    write = mem;
-    mem.clear();
+    if(k[0]==0)
+    {
+        r.write(k[1],k[2]);
+    }
+    write[core - 1] = mem[core - 1];
+    mem[core - 1].clear();
 }
--- Pipelined/cores.hpp
+++ Pipelined/cores.hpp
@@ -12,27 +12,27 @@ class ALU
 private:
   int pc1 = 0;
   int pc2 = 0;
-  int n=2;
   std::vector<std::string> program;
   std::vector<std::vector<int>> fetch;
-  std::vector<std::vector<int>> decode[2];
-  std::vector<std::vector<int>> execute[2][5];
+  std::vector<std::vector<int>> decode;
+  std::vector<std::vector<int>> execute;
   std::vector<std::vector<int>> mem;
   std::vector<std::vector<int>> write;
 
 public:
   ALU(std::pair<int, int> &p1, std::pair<int, int> &p2, int no_inst_1, int no_inst_2, memory &m, registers &r1, registers &r2, int core1, int core2);
   void executeInstruction(std::vector<int> instructions, memory &m, registers &r, int core, int &pc);
-
+  ALU();
   // Clock cycles
-  int clockCycles;
+  int clockCycles1=0;
+  int clockCycles2=0;
 
   // Function for each pipeline stage
   void instructionFetch(memory &m, int core, int &pc);
-  void instructionDecode(memory &m, int core);
-  void instructionExecute(memory &m, registers &r, int core, int &pc);
-  void memoryAccess(memory &m, int core, int &pc);
-  void writeBack(memory &m, int core, int &pc);
+  std::vector<int> instructionDecode(memory &m, int core,registers& r,int& pc);
+  std::vector<int> instructionExecute(std::vector<int>v,memory &m, registers &r, int core, int &pc);
+  void memoryAccess(std::vector<int>k,memory &m, int core, int &pc);
+  void writeBack(std::vector<int>k,memory &m, int core, int &pc, registers &r);
 
   // Helper functions for data forwarding and stall detection
   int forwardData(int reg, int stage);
