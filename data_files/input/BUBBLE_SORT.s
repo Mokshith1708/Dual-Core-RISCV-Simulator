e703da084c6285d2e8f5d359320dfc9b3bd69976
Kowshiik reddy challa <cs22b015@iittp.ac.in>
2024-04-07 23:14:10 +0530

    old version

--- data_files/input/BUBBLE_SORT.s
+++ data_files/input/BUBBLE_SORT.s
@@ -1,14 +1,17 @@
 .data
-arr: .word 20000 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+arr: .word 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
 str: .string "nibib,bububu"
+#ninin guug ig
 .text
 la x5 arr
 add x6 x5 x0 
-addi x7 x0 20
+ addi x7 x0 20
 addi x8 x0 19
 loop1:
 beq x0 x7 exit
 addi x7 x7 -1
+
+# hihifninbi
 loop2:
 addi x1 x1 1
 lw x4 0(x5)
@@ -26,3 +29,10 @@ exit:
 add x0 x0 x0
 add x1 x1 x5
 add x1 x1 x8 
+li x31 23
+li x10 1000
+addi x17 x0 1
+ecall
+la x10 str
+addi x17 x0 4
+ecall
